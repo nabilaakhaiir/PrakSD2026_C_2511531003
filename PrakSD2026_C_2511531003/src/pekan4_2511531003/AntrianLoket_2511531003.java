d9b5f2bc73c2c11bb7c00e505de52843f00b63f3
nabilaakhaiir <nabilaakhairunnisa07@gmail.com>
2026-04-29 22:00:49 +0700

    laporan praktikum pekan 4_2511531103

--- PrakSD2026_C_2511531003/src/pekan4_2511531003/AntrianLoket_2511531003.java
+++ PrakSD2026_C_2511531003/src/pekan4_2511531003/AntrianLoket_2511531003.java
@@ -1,10 +1,57 @@
 package pekan4_2511531003;
-
+import java.util.Scanner;
 public class AntrianLoket_2511531003 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 Scanner input = new Scanner(System.in);
+	        Queue_2511531003 q_1003 = new Queue_2511531003(10);
 
+	        int pilih;
+	        String nama;
+
+	        do {
+	            System.out.println("\n=== PROGRAM ANTRIAN LOKET ===");
+	            System.out.println("1. Tambah Antrian");
+	            System.out.println("2. Hapus Antrian");
+	            System.out.println("3. Tampilkan Antrian");
+	            System.out.println("4. Reverse");
+	            System.out.println("5. Keluar");
+	            System.out.print("Pilih menu: ");
+	            pilih = input.nextInt();
+	            input.nextLine();
+
+	            switch (pilih) {
+	                case 1:
+	                    System.out.print("Masukkan nama pelanggan: ");
+	                    nama = input.nextLine();
+	                    q_1003.enqueue_1003(nama);
+	                    break;
+
+	                case 2:
+	                    q_1003.dequeue_1003();
+	                    break;
+
+	                case 3:
+	                    q_1003.display_1003();
+	                    break;
+
+	                case 4:
+	                    q_1003.reverse_1003();
+	                    q_1003.display_1003();
+	                    break;
+
+	                case 5:
+	                    System.out.println("Program selesai.");
+	                    break;
+
+	                default:
+	                    System.out.println("Pilihan tidak valid!");
+	            }
+
+	        } while (pilih != 5);
+
+	        input.close();
+	    }
 	}
 
-}
+	
--- PrakSD2026_C_2511531003/src/pekan4_2511531003/Queue_2511531003.java
+++ PrakSD2026_C_2511531003/src/pekan4_2511531003/Queue_2511531003.java
@@ -1,5 +1,81 @@
 package pekan4_2511531003;
 
 public class Queue_2511531003 {
+	int front, rear, max;
+    String[] queue;
 
+    public Queue_2511531003(int max) {
+        this.max = max;
+        queue = new String[max];
+        front = -1;
+        rear = -1;
+    }
+
+    boolean isEmpty_1003() {
+        return front == -1;
+    }
+
+    boolean isFull_1003() {
+        return rear == max - 1;
+    }
+
+    void enqueue_1003(String data) {
+        if (isFull_1003()) {
+            System.out.println("Antrian penuh!");
+        } else {
+            if (isEmpty_1003()) {
+                front = 0;
+            }
+            rear++;
+            queue[rear] = data;
+            System.out.println("Data berhasil ditambahkan ke antrian");
+        }
+    }
+
+    void dequeue_1003() {
+        if (isEmpty_1003()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            System.out.println(queue[front] + " telah dilayani");
+
+            if (front == rear) {
+                front = -1;
+                rear = -1;
+            } else {
+                front++;
+            }
+        }
+    }
+
+    void display_1003() {
+        if (isEmpty_1003()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            System.out.println("Isi antrian:");
+            for (int i = front; i <= rear; i++) {
+                System.out.println((i - front + 1) + ". " + queue[i]);
+            }
+        }
+    }
+
+    void reverse_1003() {
+        if (isEmpty_1003()) {
+            System.out.println("Antrian kosong!");
+        } else {
+            int kiri = front;
+            int kanan = rear;
+
+            while (kiri < kanan) {
+                String temp = queue[kiri];
+                queue[kiri] = queue[kanan];
+                queue[kanan] = temp;
+
+                kiri++;
+                kanan--;
+            }
+
+            System.out.println("Antrian berhasil dibalik");
+        }
+    }
 }
+
